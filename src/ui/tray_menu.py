c856efeeed46ffd3dcb52ae34fb61e79f479ffec
mx57 <38256814+mx57@users.noreply.github.com>
2026-08-28 05:12:23 +0000

    Fix laser pointer menu checkmark state desynchronization
    Add laser_mode_changed signal to InputManager and connect it to TrayMenu.laser_action.setChecked to keep the tray menu checkmark in sync when laser mode changes. Add unit tests verifying the signal emission and UI action state synchronization.

--- src/ui/tray_menu.py
+++ src/ui/tray_menu.py
@@ -59,10 +59,14 @@ class TrayMenu(QObject):
         self.menu.addSeparator()
 
         # Лазерная указка
-        laser_action = QAction("Лазерная указка 🔴", self)
-        laser_action.setCheckable(True)
-        laser_action.triggered.connect(self.toggle_laser)
-        self.menu.addAction(laser_action)
+        self.laser_action = QAction("Лазерная указка 🔴", self)
+        self.laser_action.setCheckable(True)
+        self.laser_action.triggered.connect(self.toggle_laser)
+        self.menu.addAction(self.laser_action)
+
+        # Подключаем синхронизацию сигнала лазерной указки от InputManager
+        if hasattr(self.window, "input_manager") and self.window.input_manager:
+            self.window.input_manager.laser_mode_changed.connect(self.laser_action.setChecked)
 
         self.menu.addSeparator()
 
